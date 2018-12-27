f06ec5a0b4997ddba20ac918383d27782b2881f4
Riccardo Bucco <riccardobucco@gmail.com>
2018-12-27 18:59:44 +0100

    Get the box around an image

--- images.py
+++ images.py
@@ -2,6 +2,25 @@ import os
 
 from PIL import Image
 
+# Given an image (RGBA), return:
+# last_transparent_row_top,
+# last_transparent_row_bottom,
+# last_transparent_column_left,
+# last_transparent_column_right
+def _get_box(img):
+    w, h = img.size
+    pixels = list(img.getdata())
+    result = [-1, h, -1, w]
+    for index, iterable in enumerate([range(h), reversed(range(h)), range(w), reversed(range(w))]):
+        for i in iterable:
+            vec = pixels[w*i:w*(i+1)] if index < 2 else [pixels[idx*w+i] for idx in range(h)]
+            print(min([pixel[3] for pixel in vec]))
+            if max([pixel[3] for pixel in vec]) == 0:
+                result[index] = i
+            else:
+                break
+    return result
+
 # Remove all the white pixels of the images in in_folder, saving them in out_folder
 def white2transparent(in_folder, out_folder=None):
     for file in os.listdir(in_folder):
